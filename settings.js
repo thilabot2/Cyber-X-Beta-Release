8702a5098a6c5db4f8392126c95cecef0ed1dff6
thilabot2 <112172576+thilabot2@users.noreply.github.com>
2022-09-16 10:50:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94789589506'] //ur owner number
+global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ" //ur owner name
+global.ytname = "YT: RT MUSIC TUNE" //ur yt chanel name
+global.socialm = "GitHub: thila" //ur github or insta name
+global.location = "Sri Lanka, Kurunagala, Melsiripura" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94789589506']
+global.ownertag = '94789589506' //ur tag number
+global.botnumber = '94758797500' //Add your Bot Number For Log
+global.botname = 'ᴋɪɴɢ ᴅᴀʀᴋ @thila' //ur bot name
+global.ig = "https://www.youtube.com/channel/UC_aOLJqreXrsP5qciJ4xAog" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UC_aOLJqreXrsP5qciJ4xAog" //ur website to be displayed
+global.sc = 'https://github.com' //script link
+global.linkz = 'https://www.youtube.com/channel/UC_aOLJqreXrsP5qciJ4xAog'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "" //ur sticker watermark packname
+global.author = "Thila" //ur sticker watermark author
+global.footer = "®ᴋɪɴɢ ᴅᴀʀᴋ @THILA"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94789589506'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
